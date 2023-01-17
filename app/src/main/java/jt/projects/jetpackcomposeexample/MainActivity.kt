d04e983f97f318e0d932aaab81598be87dfaabeb
VladJT <onlyrus@hotmail.com>
2023-01-17 17:49:24 +0300

    add listItem

--- app/src/main/java/jt/projects/jetpackcomposeexample/MainActivity.kt
+++ app/src/main/java/jt/projects/jetpackcomposeexample/MainActivity.kt
@@ -3,58 +3,38 @@ package jt.projects.jetpackcomposeexample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.ExperimentalUnitApi
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
-import jt.projects.jetpackcomposeexample.ui.theme.JetpackComposeExampleTheme
+import androidx.compose.ui.unit.dp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            Row(
-                horizontalArrangement = Arrangement.SpaceEvenly,
-                modifier = Modifier
-                    .background(Color.LightGray)
-                    .fillMaxSize()
+            Column(
+                modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Top
             ) {
 
-                Column(
-                    modifier = Modifier
-                        .background(Color.Red)
-                        .fillMaxWidth(0.5f)
-                        .fillMaxHeight(0.5f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    showText("Hello")
-                    showText("JC")
-                    showText("Android")
-                }
+                listItem("Ivan", "manager")
+                listItem("Petr", "programmer")
+                listItem("Igor", "designer")
 
-                Column(
-                    modifier = Modifier
-                        .background(Color.Cyan)
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.5f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    showText("111")
-                    showText("222")
-                    showText("333")
-                }
             }
 
 
@@ -70,3 +50,36 @@ class MainActivity : ComponentActivity() {
 fun showText(name: String) {
     Text(text = name, fontSize = TextUnit(24f, TextUnitType.Sp))
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun listItem(name: String, profession: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp), elevation = CardDefaults.cardElevation(5.dp)
+    ) {
+        Box() {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(id = R.drawable.man),
+                    contentDescription = "avatar",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .size(100.dp)
+                        .clip(CircleShape)
+                )
+                Column(modifier = Modifier.padding(start = 16.dp)) {
+                    Text(name)
+                    Text(profession)
+                }
+
+            }
+        }
+
+    }
+}
+
